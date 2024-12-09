4f634d648fe6136ac67cfdb17c0cf4162f3a83ae
JayiaReid <jayiareid24@gmail.com>
2024-12-08 20:53:34 -0500

    .

--- books/models.py
+++ books/models.py
@@ -4,5 +4,7 @@ class Book(models.Model):
     Title = models.CharField(max_length=250)
     Author = models.CharField(max_length=250)
     ISBN = models.CharField(max_length=25)
+    year = models.CharField(max_length=4)
+    rating = models.CharField(max_length=1)
     Description = models.CharField(max_length=1000)
     Cover = models.CharField(max_length=1000)
